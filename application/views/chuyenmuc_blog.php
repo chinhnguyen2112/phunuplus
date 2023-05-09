8238abbf4f4cf67698179faecddd031c87f02bac
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-05-09 10:16:01 +0700

    Khanh update code lan 1 09/05/2023

--- application/views/chuyenmuc_blog.php
+++ application/views/chuyenmuc_blog.php
@@ -8,21 +8,21 @@
             </div>
         </div>
         <?php if (isset($content_tag) && $content_tag != '') { ?>
-            <div class="blog_content">
-                <div class="right_detail">
-                    <div class="mucluc_blog" id="mucluc_blog">
-                        <p class="title_mucluc" id="title_mucluc">Mục lục</p>
-                        <ul class="list_mucluc" id="list_mucluc">
+        <div class="blog_content">
+            <div class="right_detail">
+                <div class="mucluc_blog" id="mucluc_blog">
+                    <p class="title_mucluc" id="title_mucluc">Mục lục</p>
+                    <ul class="list_mucluc" id="list_mucluc">
 
-                        </ul>
-                    </div>
-                </div>
-                <div class="left_detail">
-                    <div class="content_blog" id="content_blog">
-                        <?= $content_tag ?>
-                    </div>
+                    </ul>
                 </div>
             </div>
+            <div class="left_detail">
+                <div class="content_blog" id="content_blog">
+                    <?= $content_tag ?>
+                </div>
+            </div>
+        </div>
         <?php } ?>
         <div class="train_content">
             <div class="top_blog">
@@ -31,15 +31,15 @@
                         <div class="blog_top">
                             <?php foreach ($blog as $key => $val) {
                                 if ($key == 0) { ?>
-                                    <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
-                                        <div class="blog_top_content blog_top_left">
-                                            <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                        </div>
-                                        <div class="blog_top_content blog_top_right">
-                                            <p class="title_blog_top"><?= $val['title'] ?></p>
-                                            <div class="sapo_blog"><?= $val['sapo'] ?></div>
-                                        </div>
-                                    </a>
+                            <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                                <div class="blog_top_content blog_top_left">
+                                    <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                                </div>
+                                <div class="blog_top_content blog_top_right">
+                                    <p class="title_blog_top"><?= $val['title'] ?></p>
+                                    <div class="sapo_blog"><?= $val['sapo'] ?></div>
+                                </div>
+                            </a>
                             <?php }
                             } ?>
                         </div>
@@ -47,21 +47,21 @@
                     <div class="list_blog">
                         <?php foreach ($blog as $key => $val) {
                             if ($key > 0) { ?>
-                                <div class="this_train">
-                                    <a href="/<?= $val['alias'] ?>/">
-                                        <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                        <div class="box_right_data">
-                                            <p class="title_blog"><?= $val['title'] ?></p>
-                                            <div class="fl_date">
-                                                <p class="cate_post"><?php $cate = chuyen_muc(['id' => $val['chuyenmuc']]);
+                        <div class="this_train">
+                            <a href="/<?= $val['alias'] ?>/">
+                                <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                                <div class="box_right_data">
+                                    <p class="title_blog"><?= $val['title'] ?></p>
+                                    <div class="fl_date">
+                                        <p class="cate_post"><?php $cate = chuyen_muc(['id' => $val['chuyenmuc']]);
                                                                     echo $cate[0]['name']; ?></p>
-                                                <span class="dot_item"></span>
-                                                <p class="date_post"><?= date('d-m-Y', $val['created_at']) ?></p> 
-                                            </div>
-                                            <div class="des_blog"><?= $val['sapo'] ?></div>
-                                        </div>
-                                    </a>
+                                        <span class="dot_item"></span>
+                                        <p class="date_post"><?= date('d-m-Y', $val['created_at']) ?></p>
+                                    </div>
+                                    <div class="des_blog"><?= $val['sapo'] ?></div>
                                 </div>
+                            </a>
+                        </div>
                         <?php }
                         } ?>
                         <div class="load_more">
@@ -73,54 +73,77 @@
                     </div>
                 </div>
                 <div class="top_right">
-                    <div class="list_news item_left">
-                        <div class="header_news header_item_left">
-                        <p>Tin Mới Nhất</p>
+                    <div class="sidebar_box">
+                        <div class="box_heading">
+                            <a href="#">Liên Kết Hữu Ích</a>
                         </div>
-                        <div class="content_news content_item_left">
-                        <ul class="list_news_content">
-                            <?php foreach ($blog as $key => $val) {
-                            if ($key < 6) { ?>
-                            <li class="item_news item">
-                            <a href="/<?= $val['alias'] ?>/"><?= $val['title'] ?></a>
-                            </li>
-                            <?php } 
-                            } ?>
-                        </ul>
+                        <div class="box_content">
+                            <ul>
+                                <li class="item_content_sidebar">
+                                    <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                                    Chuyện
+                                    <strong>
+                                        <a href="/chuyen-tinh-yeu/">Tình Yêu</a>
+                                    </strong>
+                                    Lãng Mạn
+                                </li>
+                                <li class="item_content_sidebar">
+                                    <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                                    Chăm Sóc
+                                    <strong>
+                                        <a href="/da-dep/">Da Đẹp</a>
+                                    </strong>
+                                    Như Sao Hàn
+                                </li>
+                                <li class="item_content_sidebar">
+                                    <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                                    Giá Trị Của
+                                    <strong>
+                                        <a href="/hon-nhan/">Hôn Nhân</a>
+                                    </strong>
+                                    Vợ Chồng
+                                </li>
+                                <li class="item_content_sidebar">
+                                    <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                                    Tiêu Chuẩn
+                                    <strong>
+                                        <a href="/dang-dep/">Dáng Đẹp</a>
+                                    </strong>
+                                    Phụ Nữ Việt
+                                </li>
+                                <li class="item_content_sidebar">
+                                    <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                                    Style
+                                    <strong>
+                                        <a href="/mac-dep/">Mặc Đẹp</a>
+                                    </strong>
+                                    Dành Cho Phái Nữ
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                    <div class="useful_links item_left">
-                        <div class="header_links header_item_left">
-                        <p>Liên Kết Hữu Ích</p>
+                    <div class="hot_news">
+                        <div class="box_heading">
+                            <a href="#">
+                                <img src="/images/icons/icons8-fire-30.png" />
+                                Tin Mới Nóng
+                            </a>
                         </div>
-                        <div class="content_links content_item_left">
-                        <ul class="list_links">
-                            <li class="item_links item">
-                            <img src="/images/icons/icons8-star-50.png" />
-                            <a href="#">
-                                Cập nhật 
-                                <strong>tin tức phụ nữ</strong>
-                                mới
-                                </a>
-                            </li>
-                            <li class="item_links item">
-                            <img src="/images/icons/icons8-star-50.png" />
-                            <a href="#">
-                                Đọc tin
-                                <strong>phụ nữ</strong>
-                                mới
-                                </a>
-                            </li>
-                            <li class="item_links item">
-                            <img src="/images/icons/icons8-star-50.png" />
-                            <a href="#">
-                                Cập nhật 
-                                <strong>tin tức phụ nữ</strong>
-                                mới
-                                </a>
-                            </li>
-                            
-                        </ul>
+                        <div class="box_content">
+                            <ul>
+                                <?php foreach ($blog as $key => $val) { 
+                                    if ($key > 0 && $key <= 5) { ?>
+                                <li class="item_hot_news">
+                                    <a href="/<?= $val['alias'] ?>"><?= $val['title'] ?></a>
+                                </li>
+                                <?php }
+                                } ?>
+                            </ul>
+                            <div class="see_more_news">
+                                <button class="btn_more">
+                                    <a href="/">Xem Thêm</a>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
--- application/views/result_search.php
+++ application/views/result_search.php
@@ -1,53 +1,123 @@
 <div class="container">
+    <div class="heading_left">
+        <p>Kết quả tìm kiếm với từ khóa : <?= $key_search ?></p>
+    </div>
     <div class="result_content body_width">
         <div class="left_content">
-            <div class="heading_left">
-                <p>Kết quả tìm kiếm với từ khóa : <?= $key_search ?></p>
+            <div class="result_top">
+                <div class="first_result">
+                    <a class="linl_all_detail" title="<?= $result[0]['title'] ?>" href="/<?= $result[0]['alias'] ?>/">
+                        <div class="blog_top_content blog_top_left">
+                            <img src="/<?= $result[0]['image'] ?>" alt="<?= $result[0]['title'] ?>">
+                        </div>
+                        <div class="blog_top_content blog_top_right">
+                            <p class="title_blog_top"><?= $result[0]['title'] ?></p>
+                            <div class="sapo_first"><?= $result[0]['sapo'] ?></div>
+                        </div>
+                    </a>
+                </div>
             </div>
             <div class="list_result">
-                <?php foreach ($result as $key => $val) { ?>
-                    <div class="item_result">
-                        <div class="left_item_content">
-                            <a class="img_item" href="/<?= $val['alias'] ?>/">
-                                <img src="/<?= $val['image'] ?>" />
-                            </a>
-                            <a class="name_cate" href="/<?= $val['cate_alias'] ?>/">
-                                <?= $val['cate_name'] ?>
-                            </a>
-                        </div>
-                        <div class="right_item_content">
-                            <a class="title_item_result" href="/<?= $val['alias'] ?>/"><?= $val['title'] ?></a>
+                <?php foreach ($result as $key => $val) { 
+                    if ($key > 0) { ?>
+                <div class="item_result">
+                    <a title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                        <p class="title_blog only_mobile"><?= $val['title'] ?></p>
+                    </a>
+                    <a class="linl_all_detail link_fl" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                        <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                        <div class="box_content_blog">
+                            <p class="title_blog"><?= $val['title'] ?></p>
                             <div class="fl_date">
                                 <p class="cate_post"><?php $cate = chuyen_muc(['id' => $val['chuyenmuc']]);
-                                                    echo $cate[0]['name']; ?></p>
+                                          echo $cate[0]['name']; ?></p>
                                 <span class="dot_item"></span>
-                                <p class="date_post"><?= date('d-m-Y', $val['created_at']) ?></p> 
+                                <p class="date_post"><?= date('d-m-Y', $val['created_at']) ?></p>
                             </div>
-                            <div class="sapo_blog"><?= $val['sapo'] ?></div>
+                            <span class="des_post"><?= $val['sapo'] ?></span>
                         </div>
-                    </div>
-                <?php } ?>
+                    </a>
+                </div>
+                <?php }
+                } ?>
             </div>
             <div class="pagination">
                 <?= $this->pagination->create_links() ?>
             </div>
         </div>
         <div class="right_content">
-            <div class="list_right_item">
-                <p class="title_left_content">Tin Mới Nhất</p>
-                <?php foreach ($list_news as $key => $val) { ?>
-                    <div class="item_left_content">
-                        <div class="left_item">
-                            <a class="img_item_left" href="/<?= $val['alias'] ?>/">
-                                <img src="/<?= $val['image'] ?>" />
-                            </a>
-                        </div>
-                        <div class="right_item">
-                            <a class="title_item_right" href="/<?= $val['alias'] ?>/"><?= $val['title'] ?></a>
-                            <p class="date_item_left"><?= date('d-m-Y', $val['created_at']) ?></p>
-                        </div>
+            <div class="sidebar_box">
+                <div class="box_heading">
+                    <a href="#">Liên Kết Hữu Ích</a>
+                </div>
+                <div class="box_content">
+                    <ul>
+                        <li class="item_content_sidebar">
+                            <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                            Chuyện
+                            <strong>
+                                <a href="/chuyen-tinh-yeu/">Tình Yêu</a>
+                            </strong>
+                            Lãng Mạn
+                        </li>
+                        <li class="item_content_sidebar">
+                            <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                            Chăm Sóc
+                            <strong>
+                                <a href="/da-dep/">Da Đẹp</a>
+                            </strong>
+                            Như Sao Hàn
+                        </li>
+                        <li class="item_content_sidebar">
+                            <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                            Giá Trị Của
+                            <strong>
+                                <a href="/hon-nhan/">Hôn Nhân</a>
+                            </strong>
+                            Vợ Chồng
+                        </li>
+                        <li class="item_content_sidebar">
+                            <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                            Tiêu Chuẩn
+                            <strong>
+                                <a href="/dang-dep/">Dáng Đẹp</a>
+                            </strong>
+                            Phụ Nữ Việt
+                        </li>
+                        <li class="item_content_sidebar">
+                            <img class="icon_li" src="/images/icons/icons8-star-50.png" />
+                            Style
+                            <strong>
+                                <a href="/mac-dep/">Mặc Đẹp</a>
+                            </strong>
+                            Dành Cho Phái Nữ
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="hot_news">
+                <div class="box_heading">
+                    <a href="#">
+                        <img src="/images/icons/icons8-fire-30.png" />
+                        Tin Mới Nóng
+                    </a>
+                </div>
+                <div class="box_content">
+                    <ul>
+                        <?php foreach ($list_news as $key => $val) { 
+                                    if ($key > 0 && $key < 5) { ?>
+                        <li class="item_hot_news">
+                            <a href="/<?= $val['alias'] ?>"><?= $val['title'] ?></a>
+                        </li>
+                        <?php }
+                                } ?>
+                    </ul>
+                    <div class="see_more_news">
+                        <button class="btn_more">
+                            <a href="/">Xem Thêm</a>
+                        </button>
                     </div>
-                <?php } ?>
+                </div>
             </div>
         </div>
     </div>
