3bd13f93277c1dd4a8aabf81f90639fb7d77d4ab
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-06-19 15:56:27 +0700

    Khanh update code ngay 19/06/2023 lan 1

--- application/views/chuyenmuc_blog.php
+++ application/views/chuyenmuc_blog.php
@@ -15,15 +15,23 @@
                         <div class="blog_top">
                             <?php foreach ($blog as $key => $val) {
                                 if ($key == 0) { ?>
-                            <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
-                                <div class="blog_top_content blog_top_left">
-                                    <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                </div>
-                                <div class="blog_top_content blog_top_right">
-                                    <p class="title_blog_top"><?= $val['title'] ?></p>
-                                    <div class="sapo_blog"><?= $val['sapo'] ?></div>
-                                </div>
-                            </a>
+                                    <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                                        <div class="blog_top_content blog_top_left">
+                                            <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                                        </div>
+                                        <div class="blog_top_content blog_top_right">
+                                            <p class="title_blog_top"><?= $val['title'] ?></p>
+                                            <div class="sapo_blog"><?= $val['sapo'] ?>
+                                                <p>Nếu bạn đang lo lắng và không biết việc lên thực đơn giảm cân cho người 60kg
+                                                    có khó
+                                                    hay không, hãy tham khảo bài viết dưới đây của Phụ Nữ Plus để được hướng dẫn
+                                                    chi
+                                                    tiết. Tại đây chúng tôi cung cấp cách lập kế hoạch giảm cân khoa học với
+                                                    thực đơn ăn
+                                                    kiêng lành mạnh, đồng thời mang lại hiệu quả nhanh chóng. Tìm hiểu ngay!</p>
+                                            </div>
+                                        </div>
+                                    </a>
                             <?php }
                             } ?>
                         </div>
@@ -31,21 +39,32 @@
                     <div class="list_blog">
                         <?php foreach ($blog as $key => $val) {
                             if ($key > 0) { ?>
-                        <div class="this_train">
-                            <a href="/<?= $val['alias'] ?>/">
-                                <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                <div class="box_right_data">
-                                    <p class="title_blog"><?= $val['title'] ?></p>
-                                    <div class="fl_date">
-                                        <p class="cate_post"><?php $cate = chuyen_muc(['id' => $val['chuyenmuc']]);
+                                <div class="this_train">
+                                    <a title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                                        <p class="title_blog only_mobile"><?= $val['title'] ?></p>
+                                    </a>
+                                    <a href="/<?= $val['alias'] ?>/">
+                                        <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                                        <div class="box_right_data">
+                                            <p class="title_blog"><?= $val['title'] ?></p>
+                                            <div class="fl_date">
+                                                <p class="cate_post"><?php $cate = chuyen_muc(['id' => $val['chuyenmuc']]);
                                                                         echo $cate[0]['name']; ?></p>
-                                        <span class="dot_item"></span>
-                                        <p class="date_post"><?= date('d-m-Y', $val['created_at']) ?></p>
-                                    </div>
-                                    <div class="des_blog"><?= $val['sapo'] ?></div>
+                                                <span class="dot_item"></span>
+                                                <p class="date_post"><?= date('d-m-Y', $val['created_at']) ?></p>
+                                            </div>
+                                            <div class="des_blog"><?= $val['sapo'] ?>
+                                                <p>Nếu bạn đang lo lắng và không biết việc lên thực đơn giảm cân cho người 60kg
+                                                    có khó
+                                                    hay không, hãy tham khảo bài viết dưới đây của Phụ Nữ Plus để được hướng dẫn
+                                                    chi
+                                                    tiết. Tại đây chúng tôi cung cấp cách lập kế hoạch giảm cân khoa học với
+                                                    thực đơn ăn
+                                                    kiêng lành mạnh, đồng thời mang lại hiệu quả nhanh chóng. Tìm hiểu ngay!</p>
+                                            </div>
+                                        </div>
+                                    </a>
                                 </div>
-                            </a>
-                        </div>
                         <?php }
                         } ?>
                         <div class="load_more">
