d8933195055e366677c185a4cb5d30583e1ed075
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-07-19 16:42:22 +0700

    Khanh update code ngay 19/07/2023 lan 1

--- application/views/tag.php
+++ application/views/tag.php
@@ -8,47 +8,53 @@
                         <span class="this_breadcrumb"><?= $title_page ?></span>
                     </div>
                     <?php if (isset($content_tag) && $content_tag != '') { ?>
-                        <div class="blog_content">
-                            <div class="left_detail">
-                                <div class="content_blog" id="content_blog">
-                                    <?= $content_tag ?>
-                                </div>
-                                <div class="hide_show_content">
-                                    <p class="hide_show_text">Xem thêm</p>
-                                    <div class="list_img_arrow">
-                                        <img src="/images/arrow.svg" alt="xem thêm">
-                                        <img src="/images/arrow.svg" alt="xem thêm">
-                                    </div>
+                    <div class="blog_content">
+                        <div class="left_detail">
+                            <div class="content_blog" id="content_blog">
+                                <?= $content_tag ?>
+                            </div>
+                            <div class="hide_show_content">
+                                <p class="hide_show_text">Xem thêm</p>
+                                <div class="list_img_arrow">
+                                    <img src="/images/arrow.svg" alt="xem thêm">
+                                    <img src="/images/arrow.svg" alt="xem thêm">
                                 </div>
                             </div>
                         </div>
+                    </div>
                     <?php } ?>
-                    <div class="blog_top">
-                        <?php foreach ($blog as $key => $val) {
-                            if ($key == 0) { ?>
-                                <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                    <div class="left_blog">
+                        <div class="blog_top">
+                            <?php foreach ($blog as $key => $val) {
+                                    if ($key == 0) { ?>
+                            <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                                <div class="blog_top_content blog_top_left">
                                     <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                    <div class="box_data_top">
-                                        <p class="title_blog_top"><?= $val['title'] ?></p>
-                                        <div class="sapo_blog_top"><?= $val['sapo'] ?></div>
+                                </div>
+                                <div class="blog_top_content blog_top_right">
+                                    <p class="title_blog_top"><?= $val['title'] ?></p>
+                                    <div class="sapo_blog"><?= $val['sapo'] ?>
+
                                     </div>
-                                </a>
-                        <?php }
-                        } ?>
+                                </div>
+                            </a>
+                            <?php }
+                                } ?>
+                        </div>
                     </div>
                     <div class="list_blog">
                         <?php foreach ($blog as $key => $val) {
                             if ($key > 0) { ?>
-                                <div class="this_train">
-                                    <a href="/<?= $val['alias'] ?>/">
-                                        <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                        <div class="box_right_data">
-                                            <p class="title_blog"><?= $val['title'] ?></p>
-                                            <p class="date_post"><span><?= date('d-m-Y', $val['created_at']) ?></span></p>
-                                            <div class="des_blog"><?= $val['sapo'] ?></div>
-                                        </div>
-                                    </a>
+                        <div class="this_train">
+                            <a href="/<?= $val['alias'] ?>/">
+                                <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                                <div class="box_right_data">
+                                    <p class="title_blog"><?= $val['title'] ?></p>
+                                    <p class="date_post"><span><?= date('d-m-Y', $val['created_at']) ?></span></p>
+                                    <div class="des_blog"><?= $val['sapo'] ?></div>
                                 </div>
+                            </a>
+                        </div>
                         <?php }
                         } ?>
                         <div class="load_more">
