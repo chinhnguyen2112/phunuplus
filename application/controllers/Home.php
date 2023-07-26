e5cba9624eef4f6acf39707d3995a38df08e639a
Your Name <you@example.comid.chinhnguyen@gmail.com>
2023-07-26 08:46:20 +0700

    chinh

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -50,6 +50,9 @@ class Home extends CI_Controller
     {
         $time = time();
         $alias = trim($alias);
+        if ($alias == 'nguoi-tieu-dung-thong-minh') {
+            $alias = 'tieu-dung-thong-minh';
+        }
         $data['canonical'] = base_url() . $alias . '/';
         $author = $this->Madmin->get_by(['alias' => $alias], 'admin');
         $chuyenmuc = $this->Madmin->get_by(['alias' => $alias], 'category');
