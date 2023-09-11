6d4b5244994a902e4d745d250bb227597beac1d2
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-09-11 14:19:18 +0700

    Khanh update code ngay 11/09/2023

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,96 +1,82 @@
-<?php
-$ft1 = chuyen_muc(['parent' => 1]);
-$ft2 = chuyen_muc(['parent' => 3]);
-$ft3 = chuyen_muc(['parent' => 5]);
-$ft4 = chuyen_muc(['parent' => 7]);
-$ft5 = chuyen_muc(['parent' => 10]);
-?>
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v17.0" nonce="wmm40zXP"></script>
-<div class="footer">
-    <div class="footer_content body_width">
-        <div class="ft_top item_ft">
-            <div class="item_ft_top ft_top_01">
-                <p>GÓC NHÌN ĐA CHIỀU PHỤ NỮ VIỆT NAM</p>
-                <div class="all_menu_ft">
-                    <div class="list_item_menu_ft list_left">
-                        <?php $menu_cate_parent = chuyen_muc(['parent' => 0]);
-                        foreach ($menu_cate_parent as $key => $val) {
-                            if ($key <= 5) { ?>
-                                <li class="item_menu_ft">
-                                    <a href="/<?= $val['alias'] ?>/"><?= $val['name'] ?></a>
-                                </li>
-                        <?php }
-                        } ?>
-                    </div>
-                    <div class="list_item_menu_ft list_right">
-                        <?php $menu_cate_parent = chuyen_muc(['parent' => 0]);
-                        foreach ($menu_cate_parent as $key => $val) {
-                            if ($key > 5) { ?>
-                                <li class="item_menu_ft">
-                                    <a href="/<?= $val['alias'] ?>/"><?= $val['name'] ?></a>
-                                </li>
-                        <?php }
-                        } ?>
+<?php $menu_cate_parent = chuyen_muc(['parent' => 0]);
+$count_line = 0;
+if (count($menu_cate_parent) > 2) {
+    $count_line =  count($menu_cate_parent) - 2;
+} ?>
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="footer-widgets body_width">
+        <div class="content_footer">
+            <div class="ft_left">
+                <img class="logo_ft" src="/images/logo.png" alt="logo footer">
+                <div class="widget">
+                    <p class="title_widget">GÓC NHÌN ĐA CHIỀU PHỤ NỮ VIỆT NAM</p>
+                    <div class="contact_infor">
+                        <ul class="list_contact">
+                            <li style="width:100%">
+                                <img src="/images/icons/icon_address.png" alt="icon address">
+                                <p>Số 111 đường Mễ Trì - Quận Nam Từ Liêm - Hà Nội.</p>
+                            </li>
+                            <li>
+                                <img src="/images/icons/icon_website.png" alt="icon website">
+                                <p>phunuso.vn -</p>
+                            </li>
+                            <li style="margin-left:3px;width:51%">
+                                <img src="/images/icons/icon_mail.png" alt="icon email">
+                                <p>cs.phunuso@gmail.com</p>
+                            </li>
+                            <li>
+                                <img src="/images/icons/icon_facebook.png" alt="icon facebook">
+                                <p>fb.com/phunuso.vn</p>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
-            <div class="line_ft_top"></div>
-            <div class="item_ft_top ft_top_02">
-                <a href="/">
-                    <img src="/images/logo.png" alt="logo">
-                </a>
-                <p>All rights reserved © Phụ Nữ Plus</p>
-                <p><b>Phụ Nữ Plus</b> Web chia sẻ kiến thức và kinh nghiệm hữu ích cho phụ nữ về nhiều lĩnh vực đời sống về Yêu, Làm đẹp, Vào bếp, Sức khỏe, Tiêu dùng, Phóng sự.</p>
-            </div>
-            <div class="line_ft_top line_nonpc"></div>
-            <div class="item_ft_top ft_top_03">
-                <div class="fanpage_fb">
-                    <div class="fb-page" data-href="https://www.facebook.com/phunutre.vn" data-tabs="" data-width="482" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false">
-                        <blockquote cite="https://www.facebook.com/phunutre.vn" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/phunutre.vn">Phụ Nữ News</a></blockquote>
+            <div class="ft_right">
+                <div class="r_01">
+                    <p class="title_r">CHUYÊN MỤC</p>
+                    <div class="list_links">
+                        <?php $menu_cate_parent = chuyen_muc('parent = 0 AND id != 42 AND id != 41');
+                        foreach ($menu_cate_parent as $val) {
+                            $menu_cate = chuyen_muc(['parent' => $val['id']]); ?>
+                            <div class="item_link_ft" id="this_menu">
+                                <span onclick="big_item_menu(this,1)">
+                                    <a href="/<?= $val['alias'] ?>/">
+                                        <img src="/images/icons/icon_ar.png" alt="icon arrow footer">
+                                        <?= $val['name'] ?>
+                                    </a>
+                                </span>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
-                <div class="list_contact">
-                    <img class="qr_code" src="/images/icons/qrcode-new.png" alt="qr code">
-                    <div class="app">
-                        <a href="#">
-                            <img src="/images/icons/googleplay.png" alt="google play">
-                        </a>
-                        <a href="#">
-                            <img src="/images/icons/appstore.png" alt="app store">
-                        </a>
-                    </div>
-                    <div class="mxh">
-                        <a href="#">
-                            <img class="icon_fb" src="/images/icons/icon_fb_ft.png" alt="icon facebook">
-                        </a>
-                        <a href="#">
-                            <img class="icon_yt" src="/images/icons/icon_yt_ft.png" alt="icon youtube">
-                        </a>
-                        <a href="#">
-                            <img class="icon_tt" src="/images/icons/icon_tt_ft.png" alt="icon tiktok">
-                        </a>
-                        <a href="#">
-                            <img class="icon_ins" src="/images/icons/icon_ins_ft.png" alt="icon instagram">
-                        </a>
-                    </div>
-                    <a rel="nofollow" href="https://www.dmca.com/Protection/Status.aspx?id=d2441786-b99f-4f19-804a-7eb799b566ba&refurl=https%3a%2f%2fphunuplus.vn%2f&rlo=true"><img src="https://images.dmca.com/Badges/dmca_protected_sml_120am.png?ID=d2029178-63cc-4769-84c4-4b39e24fc3f1" alt="DMCA.com Protection Status"></a>
+                <div class="r_02">
+                    <p class="title_r">ĐĂNG KÝ NHẬN TIN</p>
+                    <form class="form_email" id="newsletter">
+                        <p><strong>Đăng ký nhận bản tin</strong> , bài viết tư vấn câp nhật hằng ngày.</p>
+                        <input class="input_text" type="email" name="email" placeholder="Email Address" required>
+                        <input class="btn_submit_form" value="SIGN UP" type="submit">
+                    </form>
                 </div>
-            </div>
-        </div>
-        <div class="ft_bot item_ft">
-            <div class="item_ft_bot ft_bot_01">
-                <p><b>Hợp tác nội dung:</b> Lưu Lan Hương</p>
-                <p><b>Hợp tác thương mại:</b> Phụ Nữ News</p>
-            </div>
-            <div class="item_ft_bot ft_bot_02">
-                <p><b>Chịu trách nhiệm nội dung:</b> Cao Minh Tuệ</p>
-                <p><b>Điện thoại:</b> 0328 39 635</p>
-            </div>
-            <div class="item_ft_bot ft_bot_03">
-                <p><b>Trụ sở chính:</b> Số 111 đường Mễ Trì - Quận Nam Từ Liêm - Hà Nội.</p>
-                <p><b>Email:</b> support@manta.vn</p>
             </div>
         </div>
     </div>
-</div>
+    <div class="copyright">
+        <div class="content_copy body_width">
+            <div class="site-info"> © 2022 Phụ Nữ Plus</div>
+            <div class="list_link_bottom">
+                <ul>
+                    <li>
+                        <a href="#">Giới thiệu</a>
+                    </li>
+                    <li>
+                        <a href="#">Liên hệ</a>
+                    </li>
+                    <li>
+                        <a href="#">Chính sách</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
